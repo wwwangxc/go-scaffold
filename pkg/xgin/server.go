7dbd6409a7274ccc43bcb1bf3d92d7bb36cf013c
xingChen <xingchenw@keyayun.com>
2020-09-07 17:05:55 +0800

    修改优雅关机机制，保证服务启动时发生异常，可将该异常记录

--- pkg/xgin/server.go
+++ pkg/xgin/server.go
@@ -22,18 +22,23 @@ type HTTPServer struct {
 // ListenAndServe ..
 func (t *HTTPServer) ListenAndServe() {
 	fmt.Printf("Listening and serving HTTP on %s\n", t.Addr)
-	go t.Server.ListenAndServe()
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Info("Shutdown Server ...")
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		time.Duration(t.conf.ShutdownTTL)*time.Second)
-	defer cancel()
-	if err := t.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown: ", zap.Error(err))
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		log.Info("Shutdown Server ...")
+		ctx, cancel := context.WithTimeout(
+			context.Background(),
+			time.Duration(t.conf.ShutdownTTL)*time.Second)
+		defer cancel()
+		if err := t.Shutdown(ctx); err != nil {
+			log.Fatal("Server Shutdown: ", zap.Error(err))
+		}
+		log.Info("Server exiting...")
+	}()
+
+	if err := t.Server.ListenAndServe(); err != nil {
+		log.Error(err.Error())
 	}
-	log.Info("Server exiting...")
 }
