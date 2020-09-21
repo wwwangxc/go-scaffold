3c84c5c5951197592f5f9340d5a362541e4f2cb7
xingChen <xingchenw@keyayun.com>
2020-09-21 14:43:21 +0800

    增加logger盘空

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -10,35 +10,56 @@ var (
 
 // Sync ..
 func Sync() {
-	logger.Sync()
+	if logger == nil {
+		return
+	}
+	_ = logger.Sync()
 }
 
 // Debug ..
 func Debug(msg string, fields ...zap.Field) {
+	if logger == nil {
+		return
+	}
 	logger.Debug(msg, fields...)
 }
 
 // Info ..
 func Info(msg string, fields ...zap.Field) {
+	if logger == nil {
+		return
+	}
 	logger.Info(msg, fields...)
 }
 
 // Warn ..
 func Warn(msg string, fields ...zap.Field) {
+	if logger == nil {
+		return
+	}
 	logger.Warn(msg, fields...)
 }
 
 // Error ..
 func Error(msg string, fields ...zap.Field) {
+	if logger == nil {
+		return
+	}
 	logger.Error(msg, append(fields, zap.Stack("stack"))...)
 }
 
 // Panic ..
 func Panic(msg string, fields ...zap.Field) {
+	if logger == nil {
+		return
+	}
 	logger.Panic(msg, fields...)
 }
 
 // Fatal ..
 func Fatal(msg string, fields ...zap.Field) {
+	if logger == nil {
+		return
+	}
 	logger.Fatal(msg, fields...)
 }
