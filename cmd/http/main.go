f597299a0e0896a48bddf8b68edc61253275837b
wls_ubuntu <w.xingchen@outlook.com>
2020-09-26 00:17:12 +0800

    修改暴露的http启动函数

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -4,25 +4,11 @@ package main
 
 import (
 	"fmt"
-	"go-scaffold/internal/constant"
 	"go-scaffold/internal/http"
-	"go-scaffold/pkg/conf"
-	"go-scaffold/pkg/log"
-	"go-scaffold/pkg/xgorm"
-	"go-scaffold/pkg/xredis"
 	"os"
 )
 
 func main() {
 	fmt.Printf("processID: %d\n", os.Getppid())
-	conf.Init()                                                                            // 加载配置文件
-	log.RawConfig("app.log", conf.GetHandler()).Init()                                     // 加载日志
-	defer log.Sync()                                                                       // 日志落盘
-	xgorm.RawConfig("app.mysql.db1", conf.GetHandler()).Append(constant.MySQLStoreNameDB1) // 加载gorm
-	xgorm.RawConfig("app.mysql.db2", conf.GetHandler()).Append(constant.MySQLStoreNameDB2) // 加载gorm
-	defer xgorm.CloseAll()
-	xredis.RawConfig("app.redis.0", conf.GetHandler()).Append(constant.RedisStoreNameDB0) // 加载Redis 0
-	xredis.RawConfig("app.redis.1", conf.GetHandler()).Append(constant.RedisStoreNameDB1) // 加载Redis 1
-	defer xredis.CloseAll()
-	http.Serve() // 启动服务
+	http.Run()
 }
--- internal/http/http.go
+++ internal/http/http.go
@@ -1,23 +1,35 @@
 package http
 
 import (
+	"go-scaffold/internal/constant"
 	"go-scaffold/internal/http/handler"
 	"go-scaffold/internal/http/middleware"
 	"go-scaffold/pkg/conf"
+	"go-scaffold/pkg/log"
 	"go-scaffold/pkg/xgin"
+	"go-scaffold/pkg/xgorm"
+	"go-scaffold/pkg/xredis"
 )
 
-// Serve ..
-func Serve() {
-	xgin.RawConfig("app.http", conf.GetHandler()).Setup(
-		// middleware
-		middleware.Logger,
-		middleware.Recovery,
-		middleware.Authentication,
-		middleware.Swagger,
-
-		// register handler
-		handler.RoutePing,
-		handler.RouteAuthentication,
-	).Build().ListenAndServe()
+func Run() {
+	conf.Init()                                                                            // 加载配置文件
+	log.RawConfig("app.log", conf.GetHandler()).Init()                                     // 加载日志
+	defer log.Sync()                                                                       // 日志落盘
+	xgorm.RawConfig("app.mysql.db1", conf.GetHandler()).Append(constant.MySQLStoreNameDB1) // 加载gorm
+	xgorm.RawConfig("app.mysql.db2", conf.GetHandler()).Append(constant.MySQLStoreNameDB2) // 加载gorm
+	defer xgorm.CloseAll()
+	xredis.RawConfig("app.redis.0", conf.GetHandler()).Append(constant.RedisStoreNameDB0) // 加载Redis 0
+	xredis.RawConfig("app.redis.1", conf.GetHandler()).Append(constant.RedisStoreNameDB1) // 加载Redis 1
+	defer xredis.CloseAll()
+	xgin.RawConfig("app.http", conf.GetHandler()).
+		WithMiddlewares(
+			middleware.Logger,
+			middleware.Recovery,
+			middleware.Authentication,
+			middleware.Swagger,
+		).
+		WithRoutes(
+			handler.RoutePing,
+			handler.RouteAuthentication,
+		).Build().ListenAndServe()
 }
